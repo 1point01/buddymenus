e0114442d981aa2b5a4a5879202e04fc22b8629b
meetmthosar <114148836+meetmthosar@users.noreply.github.com>
2023-06-03 18:43:29 +0530

    Changed buddymenu web file

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use BuilderBuddy\BuddyMenus\Models\BuddyMenu;
 use BuilderBuddy\BuddyMenus\Http\Controllers\BuddyMenuController;
 
-$menus = BuddyMenu::all();
+/*$menus = BuddyMenu::all();
 
 foreach ($menus as $menu) {
     Route::get($menu->routes, $menu->controllers)->name($menu->route_name);
-}
+}*/
 
 Route::group(['namespace' => 'BuilderBuddy\BuddyMenus\Http\Controllers', 'middleware' => ['web']], function () {
     Route::resource('buddymenus', BuddyMenuController::class);
